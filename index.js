5171ed87fce56cb14ff75ecfa1bd252fbf8776a7
Jean-Christophe Hoelt <hoelt@fovea.cc>
2024-02-08 10:25:12 +0200

    Attempt to add the text completion endpoint
    But for some reason, this does not work with copilot...

--- index.js
+++ index.js
@@ -5,12 +5,15 @@ const { randomUUID } = require('crypto');
 const app = express();
 const port = parseInt(process.env.PORT || '3301');
 
+const COMPLETE_MARK = '<REPLACE' + '_' + 'THIS>';
+
 const owned_by = {
   'mistral': 'Mistral AI',
   'mixtral': 'Mistral AI',
   'llama2': 'Meta Platforms',
   'codellama': 'Meta Platforms',
   'phi': 'Microsoft',
+  // more
 };
 
 // Configure Bunyan logger
@@ -22,7 +25,7 @@ app.get('/v1/models', async (req, res) => {
   let req_id = req.headers['x-request-id'] || randomUUID();
   if (typeof req_id !== 'string') req_id = req_id.toString();
   const log = logger.child({req_id});
-  log.info({ body: req.body }, 'Request received.');
+  log.info({ url: req.url, body: req.body }, 'Request received.');
   try {
     const options = {
       url: 'http://127.0.0.1:11434/api/tags',
@@ -51,11 +54,20 @@ app.get('/v1/models', async (req, res) => {
   }
 });
 
-app.post('/v1/chat/completions', async (req, res) => {
+
+/**
+ * Handles chat completion requests.
+ * 
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @param {string} forceModel - The model to force for completion (optional).
+ * @returns {Promise<void>} - A promise that resolves when the completion is handled.
+ */
+async function chatCompletion(req, res, forceModel) {
   let req_id = req.headers['x-request-id'] || randomUUID();
   if (typeof req_id !== 'string') req_id = req_id.toString();
   const log = logger.child({req_id});
-  log.info({ body: req.body }, 'Request received.');
+  log.info({ url: req.url, body: req.body }, 'Request received.');
   try {
     const options = {
       url: 'http://127.0.0.1:11434/api/chat',
@@ -65,12 +77,17 @@ app.post('/v1/chat/completions', async (req, res) => {
       },
       data: {
         ...req.body,
+        model: req.body.model || forceModel,
         stream: false,
+        options: {
+          temperature: req.body.temperature || undefined,
+          // test
+        }
       }
     };
 
     let result = await makeRequest(log, options);
-    log.info({ body: result }, 'Response');
+    log.info({ body: result.message }, 'Response');
     res.status(200).json({
       id: req_id,
       created: +new Date(result.created_at),
@@ -95,6 +112,165 @@ app.post('/v1/chat/completions', async (req, res) => {
     // Log error
     log.error(`Error processing request: ${error}`);
   }
+}
+
+/**
+ * Handles generative completion requests (alternative implementation using chat)
+ * 
+ * @see {@link https://platform.openai.com/docs/api-reference/completions/create}
+ * 
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @param {string} forceModel - The model to force for completion (optional).
+ * @returns {Promise<void>} - A promise that resolves when the completion is handled.
+ */
+async function engineCompletionsAlt(req, res, forceModel) {
+  let req_id = req.headers['x-request-id'] || randomUUID();
+  if (typeof req_id !== 'string') req_id = req_id.toString();
+  const log = logger.child({req_id});
+  log.info({ url: req.url, ...req.body }, 'Engine Completions Alt: Request received.');
+  // log.info('Engine Completions Alt: Request received.');
+  try {
+    const messages = [];
+    const {extra, prompt,suffix} = req.body;
+    let language = extra && extra.language || 'text';
+    messages.push({
+      role: 'system',
+      content: `Your role is to complete a block of ${language}. I'll send you the code with a ${COMPLETE_MARK} tag. Respond with the text that goes in place of ${COMPLETE_MARK}.`
+    });
+    if (!suffix) {
+      messages.push({
+        role: 'user',
+        content: `${prompt} ${COMPLETE_MARK}`,
+      });
+    }
+    else {
+      messages.push({
+        role: 'user',
+        content: `${prompt} ${COMPLETE_MARK}\n${suffix}`,
+      });
+    }
+    messages.push({
+      role: 'user',
+      content: `Remember, you should only respond with the actual block that has to be inserted in place of ${COMPLETE_MARK}. Do not make a sentence. Just respond with this text.`
+    });
+    const options = {
+      url: 'http://127.0.0.1:11434/api/chat',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      data: {
+        model: forceModel || req.body.model,
+        messages,
+        options: {
+          temperature: req.body.temperature || undefined,
+        },
+        stream: false,
+      }
+    };
+
+    let result = await makeRequest(log, options);
+    const prompt_tokens = result.prompt_eval_count;
+    const completion_tokens = result.eval_count;
+    const response = {
+      id: 'cmpl-' + req_id,
+      object: "text_completion",
+      created: +new Date(result.created_at),
+      model: result.model,
+      system_fingerprint: `openollamai:${result.model}`,
+      choices: [{
+        text: result.message.content + req.body.stop[0],
+        index: 0,
+        logprobs: null,
+        finish_reason: "length" // result.done ? "stop" : "length",
+      }],
+      usage: {
+        prompt_tokens,
+        completion_tokens,
+        total_tokens: prompt_tokens + completion_tokens,
+      },
+    };
+    log.info({ response }, 'Response to user request');
+    res.status(200).json(response);
+  } catch (error) {
+    log.warn(error);
+    res.status(400).end();
+
+    // Log error
+    log.error(`Error processing request: ${error}`);
+  }
+}
+/**
+ * Handles generative completion requests.
+ * 
+ * @see {@link https://platform.openai.com/docs/api-reference/completions/create}
+ * 
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @param {string} forceModel - The model to force for completion (optional).
+ * @returns {Promise<void>} - A promise that resolves when the completion is handled.
+ */
+async function engineCompletions(req, res, forceModel) {
+  let req_id = req.headers['x-request-id'] || randomUUID();
+  if (typeof req_id !== 'string') req_id = req_id.toString();
+  const log = logger.child({req_id});
+  log.info({ url: req.url, body: req.body }, 'Request received.');
+  try {
+    const options = {
+      url: 'http://127.0.0.1:11434/api/generate',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      data: {
+        ...req.body,
+        stream: false,
+      }
+    };
+    if (forceModel) {
+      options.data.model = forceModel;
+    }
+
+    let result = await makeRequest(log, options);
+    log.info({ response: result.response }, 'Response');
+    res.status(200).json({
+      id: req_id,
+      object: "text_completion",
+      created: +new Date(result.created_at),
+      model: result.model,
+      system_fingerprint: "ollama-openai-bridge",
+      choices: [{
+        index: 0,
+        text: result.response,
+        finish_reason: result.done ? "stop" : "length",
+      }],
+      usage: {
+        // prompt_tokens: result.prompt_tokens,
+        // completion_tokens: result.eval_count,
+        // total_tokens: result.eval_count,
+      },
+      // data: result
+    });
+  } catch (error) {
+    log.warn(error);
+    res.status(400).end();
+
+    // Log error
+    log.error(`Error processing request: ${error}`);
+  }
+}
+
+app.post('/v1/completions', async (req, res) => {
+  engineCompletionsAlt(req, res);
+});
+
+app.post('/v1/engines/:engine/completions', async (req, res) => {
+  engineCompletionsAlt(req, res, req.params.engine);
+});
+
+app.post('/v1/chat/completions', async (req, res) => {
+  chatCompletion(req, res, null);
 });
 
 app.use((req, res) => {
@@ -109,7 +285,15 @@ app.listen(port, () => {
 });
 
 async function makeRequest(log, options) {
-  const response = await axios(options);
-  // log.info({data: response.data}, 'Response from API');
-  return response.data;
+  try {
+    log.info(options, 'Request to API');
+    const response = await axios(options);
+    log.info(response.data, 'Response from API');
+    return response.data;
+  }
+  catch (err) {
+    log.error(err, 'API call failed');
+    throw err;
+    // 
+  }
 } 
